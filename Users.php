b60ad1196fb0b7ec94d127c794fbe69659718083
lamatipanontM <lamatipanont.m@oundleschool.org.uk>
2023-04-19 10:22:46 +0100

    test

---
+++ Users.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <tittle>USERS</tittle>
+</head>
+<body>
+
+</body>
+</html>
+
+<form action="addusers.php">
+    Email:<input type="text" name="email"><br>
+    Username:<input type="text" name="username"><br>
+    Password:<input type="password" name="password"><br>
+    <inpyut type ="submit" value="Sign up">
+</form>
